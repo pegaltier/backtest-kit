566c475f1eb2353943cf0d2b58010c9351b268ab
Petr Tripolsky <tripolskypetr@gmail.com>
2026-01-16 20:35:49 +0500

    docs

--- scripts/uml.mjs
+++ scripts/uml.mjs
@@ -8,7 +8,7 @@ import * as moduleData from "../build/index.mjs";
 
 const UML_STEP = "\t";
 const UML_BULLET = "•";
-const MAX_NESTING = 8;
+const MAX_NESTING = 16;
 
 const toUML = async () => {
   const getNodes = (targetObj) =>
