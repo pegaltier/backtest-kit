facee445efd84fbf865ff895fcfbb0bd2e5c9093
Petr Tripolsky <tripolskypetr@gmail.com>
2026-01-27 18:50:21 +0500

    backend

---
+++ packages/front/src/config/params.ts
@@ -0,0 +1,3 @@
+export const CC_WWWROOT_PATH = "./public";
+export const CC_WWWROOT_HOST = "0.0.0.0";
+export const CC_WWWROOT_PORT = 60050;
---
+++ packages/front/src/function/serve.function.ts
@@ -0,0 +1,44 @@
+import http from "http";
+import { singleshot } from "functools-kit";
+
+import { CC_WWWROOT_HOST, CC_WWWROOT_PORT } from "../config/params";
+
+import router from "../config/router";
+import ioc from "../lib";
+
+const METHOD_NAME_SERVE = "serve.serve";
+const METHOD_NAME_GET_ROUTER = "serve.getRouter";
+
+const MAX_CONNECTIONS = 1_000;
+const SOCKET_TIMEOUT = 60 * 10 * 1000;
+
+const serveInternal = singleshot(
+  (host = CC_WWWROOT_HOST, port = CC_WWWROOT_PORT) => {
+    const server = new http.Server(router);
+
+    server.listen(port, host).addListener("listening", () => {
+      console.log(`Listening on http://${host}:${port}`);
+    });
+
+    server.maxConnections = MAX_CONNECTIONS;
+    server.setTimeout(SOCKET_TIMEOUT);
+
+    return () => {
+      server.close();
+      serveInternal.clear();
+    };
+  },
+);
+
+export function serve(host?: string, port?: number) {
+  ioc.loggerService.log(METHOD_NAME_SERVE, {
+    host,
+    port,
+  });
+  return serveInternal(host, port);
+}
+
+export function getRouter() {
+  ioc.loggerService.log(METHOD_NAME_GET_ROUTER);
+  return router;
+}
