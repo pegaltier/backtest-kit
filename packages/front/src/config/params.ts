e0c2c7fb10812e865c295c8ed807ad836c54bb22
Petr Tripolsky <tripolskypetr@gmail.com>
2026-01-27 19:17:01 +0500

    wip

--- packages/front/src/config/params.ts
+++ packages/front/src/config/params.ts
@@ -1,8 +1,13 @@
+import { createRequire } from "module";
+import { join } from "path";
+
+const require = createRequire(import.meta.url);
 
 function getPublicPath() {
-    
+    const modulePath = require.resolve('@backtest-kit/ui');
+    return join(modulePath, "../public");
 }
 
-export const CC_WWWROOT_PATH = "./public";
+export const CC_WWWROOT_PATH = "./public"; // getPublicPath()
 export const CC_WWWROOT_HOST = "0.0.0.0";
 export const CC_WWWROOT_PORT = 60050;
