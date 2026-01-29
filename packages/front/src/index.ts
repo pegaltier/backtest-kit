1036fdf62817d62b081d7f0d0e03c49a5dd6c089
Petr Tripolsky <tripolskypetr@gmail.com>
2026-01-30 00:13:58 +0500

    patch

--- packages/front/src/index.ts
+++ packages/front/src/index.ts
@@ -3,4 +3,7 @@ export { setLogger } from "./function/setup.function";
 
 export { SymbolModel } from "./model/Symbol.model";
 
+export { getModulesPath } from "./helpers/getModulesPath";
+export { getPublicPath } from "./helpers/getPublicPath";
+
 export { ioc as lib } from "./lib";
