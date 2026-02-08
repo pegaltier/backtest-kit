04ff6941c948f41911305be15f5b20617eb6b4a7
Petr Tripolsky <tripolskypetr@gmail.com>
2026-02-08 14:23:44 +0500

    wip

--- packages/front/modules/frontend/src/hooks/useRiskView/components/CopyIcon/CopyIcon.tsx
+++ packages/front/modules/frontend/src/hooks/useRiskView/components/CopyIcon/CopyIcon.tsx
@@ -6,88 +6,79 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 
 import ContentCopy from "@mui/icons-material/ContentCopy";
-import { ActionIcon, copyToClipboard, createAwaiter, debounce } from "react-declarative";
-
-const TOOLTIP_CLOSE_DELAY = 800;
+import {
+    ActionIcon,
+    copyToClipboard,
+    createAwaiter,
+    debounce,
+} from "react-declarative";
+import ioc from "../../../../lib";
 
 interface ICopyIconProps {
-  disabled?: boolean;
-  className?: string;
-  style?: React.CSSProperties;
-  sx?: SxProps<any>;
-  delay?: number;
-  onClick: (e: React.MouseEvent<HTMLButtonElement>, doCopy: (content: string | number) => void) => void | Promise<void>;
-  color?: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
+    disabled?: boolean;
+    className?: string;
+    style?: React.CSSProperties;
+    sx?: SxProps<any>;
+    onClick: (
+        e: React.MouseEvent<HTMLButtonElement>,
+        doCopy: (content: string | number) => void,
+    ) => void | Promise<void>;
+    color?:
+        | "inherit"
+        | "primary"
+        | "secondary"
+        | "success"
+        | "error"
+        | "info"
+        | "warning";
 }
 
 const doCopy = async (content: React.ReactNode) => {
-  let isOk = false;
-  isOk = isOk || typeof content === "string";
-  isOk = isOk || typeof content === "number";
-  isOk = isOk || typeof content === "boolean";
-  isOk = isOk || content === undefined;
-  isOk = isOk || content === null;
-  if (!isOk) {
-    return;
-  }
-  await copyToClipboard(String(content));
+    let isOk = false;
+    isOk = isOk || typeof content === "string";
+    isOk = isOk || typeof content === "number";
+    isOk = isOk || typeof content === "boolean";
+    isOk = isOk || content === undefined;
+    isOk = isOk || content === null;
+    if (!isOk) {
+        return;
+    }
+    await copyToClipboard(String(content));
 };
 
 export const CopyIcon = ({
-  disabled,
-  className,
-  style,
-  sx,
-  onClick,
-  delay = TOOLTIP_CLOSE_DELAY,
-  color = "info",
+    disabled,
+    className,
+    style,
+    sx,
+    onClick,
+    color,
 }: ICopyIconProps) => {
-  const [open, setOpen] = useState(false);
-
-  const emitClose = useMemo(
-    () =>
-      debounce(() => {
-        setOpen(false);
-      }, delay),
-    []
-  );
-
-  useEffect(() => () => emitClose.clear(), []);
-
-  return (
-    <Tooltip
-      className={className}
-      style={style}
-      sx={sx}
-      open={open}
-      title="Copied!"
-      placement="bottom"
-      arrow
-      disableFocusListener
-      disableTouchListener
-    >
-      <ActionIcon
-        disabled={disabled}
-        color={color}
-        onClick={async (e) => {
-          const [awaiter, { resolve }] = createAwaiter<string | number>();
-          {
-            e.preventDefault();
-            e.stopPropagation();
-          }
-          await onClick(e, (content) => resolve(content));
-          const content = await awaiter;
-          {
-            setOpen(true);
-            await doCopy(content);
-            emitClose();
-          }
-        }}
-      >
-        <ContentCopy />
-      </ActionIcon>
-    </Tooltip>
-  );
+    return (
+        <ActionIcon
+            className={className}
+            style={style}
+            sx={sx}
+            disabled={disabled}
+            color={color}
+            size={36}
+            onClick={async (e) => {
+                const [awaiter, { resolve }] = createAwaiter<string | number>();
+                {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+                await onClick(e, (content) => resolve(content));
+                const content = await awaiter;
+                {
+                    await doCopy(content);
+                }
+                ioc.alertService.notify("Copied!");
+            }}
+        >
+            <ContentCopy />
+        </ActionIcon>
+    );
 };
 
 export default CopyIcon;
