2fde3a86e196ff5b30849b07d298b1c2f63d9814
Petr Tripolsky <tripolskypetr@gmail.com>
2026-02-26 19:16:00 +0500

    cli-leverage

--- src/index.ts
+++ src/index.ts
@@ -263,6 +263,7 @@ export {
   TrailingTakeCommit,
   BreakevenCommit,
   ActivateScheduledCommit,
+  AverageBuyCommit,
 } from "./contract/StrategyCommit.contract";
 
 export { ExecutionContextService } from "./lib/services/context/ExecutionContextService";
