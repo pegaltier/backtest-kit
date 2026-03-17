7b9a52d64b117c9176f645e303817e3f5ae805c9
Petr Tripolsky <tripolskypetr@gmail.com>
2026-03-17 20:58:56 +0500

    wip

--- packages/front/modules/frontend/src/hooks/useFileContentView/useFileContentView.tsx
+++ packages/front/modules/frontend/src/hooks/useFileContentView/useFileContentView.tsx
@@ -5,20 +5,36 @@ import {
     History,
     useActualRef,
     ActionIcon,
+    Async,
 } from "react-declarative";
-import { Close } from "@mui/icons-material";
+import { ArrowBack, Close, Download, Print } from "@mui/icons-material";
 import { createMemoryHistory } from "history";
+import { Box, Stack } from "@mui/material";
 import routes from "./routes";
 import { CC_FULLSCREEN_SIZE_REQUEST } from "../../config/params";
 import tabs from "./tabs";
 import ioc from "../../lib";
+import CopyIcon from "./components/CopyIcon";
+import downloadMarkdown from "../../utils/downloadMarkdown";
 
 const history = createMemoryHistory();
 
 const fetchData = async (id: string) => {
     const file = await ioc.explorerViewService.getFileInfo(id);
     const content = await ioc.explorerViewService.getFileContent(file.path);
-    return { content, mimeType: file.mimeType };
+    return { content, mimeType: file.mimeType, label: file.label };
+};
+
+const handleDownload = async (id: string) => {
+    const { content, label } = await fetchData(id);
+    const blob = new Blob([content], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    ioc.layoutService.downloadFile(url, label);
+};
+
+const handleCopy = async (id: string, onCopy: (content: string) => void) => {
+    const { content } = await fetchData(id);
+    onCopy(content);
 };
 
 export const useFileContentView = () => {
@@ -34,6 +50,13 @@ export const useFileContentView = () => {
         setPathname(path);
     };
 
+    const handlePrint = async () => {
+        const { content, mimeType } = await fetchData(id$.current);
+        if (mimeType === "text/markdown") {
+            await downloadMarkdown(content);
+        }
+    };
+
     const { pickData, render } = useTabsModal({
         tabs,
         withStaticAction: true,
@@ -43,10 +66,52 @@ export const useFileContentView = () => {
         sizeRequest: CC_FULLSCREEN_SIZE_REQUEST,
         history,
         routes,
+        BeforeTitle: ({ onClose }) => {
+            const { total } = useModalManager();
+            return (
+                <Box
+                    sx={{
+                        mr: 1,
+                        display: total === 1 ? "none" : "flex",
+                    }}
+                >
+                    <ActionIcon onClick={onClose}>
+                        <ArrowBack />
+                    </ActionIcon>
+                </Box>
+            );
+        },
         AfterTitle: ({ onClose }) => (
-            <ActionIcon onClick={onClose}>
-                <Close />
-            </ActionIcon>
+            <Stack direction="row" gap={1}>
+                <Async>
+                    {async () => {
+                        const { mimeType } = await ioc.explorerViewService.getFileInfo(id$.current);
+                        if (mimeType !== "text/markdown") {
+                            return null;
+                        }
+                        return (
+                            <ActionIcon
+                                sx={{ mr: "10px" }}
+                                onClick={() => handlePrint()}
+                            >
+                                <Print />
+                            </ActionIcon>
+                        )
+                    }}
+                </Async>
+                <CopyIcon
+                    onClick={async (_, onCopy) => {
+                        await handleCopy(id$.current, onCopy);
+                    }}
+                    sx={{ mr: "10px", mt: "2.5px" }}
+                />
+                <ActionIcon onClick={() => handleDownload(id$.current)}>
+                    <Download />
+                </ActionIcon>
+                <ActionIcon onClick={onClose}>
+                    <Close />
+                </ActionIcon>
+            </Stack>
         ),
         fetchState: async () => await fetchData(id$.current),
         mapInitialData: ([{ content, mimeType }]) => ({
