926b1554327049f6789ef4743ebd0d27c1fca9c6
Petr Tripolsky <tripolskypetr@gmail.com>
2026-03-18 12:16:15 +0500

    wip

--- packages/front/modules/frontend/src/lib/services/helpers/MarkdownHelperService.ts
+++ packages/front/modules/frontend/src/lib/services/helpers/MarkdownHelperService.ts
@@ -1,12 +1,23 @@
-import { IField, FieldType, getAvailableFields } from "react-declarative";
+import { IField, FieldType, getAvailableFields, inject } from "react-declarative";
 import { get } from "lodash";
+import LoggerService from "../base/LoggerService";
+import TYPES from "../../core/TYPES";
+import downloadMarkdown from "../../../utils/downloadMarkdown";
 
 export class MarkdownHelperService {
-    buildMarkdownFromFields = <Data extends object = any>(
+
+    private readonly loggerService = inject<LoggerService>(TYPES.loggerService);
+
+    public buildMarkdownFromFields = <Data extends object = any>(
         fields: IField[],
         data: Data,
         payload: Record<string, any> = {},
     ): string => {
+        this.loggerService.log("markdownHelperService buildMarkdownFromFields", {
+            fields,
+            data,
+        });
+
         const lines: string[] = [];
 
         const { visible } = getAvailableFields(
@@ -58,6 +69,19 @@ export class MarkdownHelperService {
 
         return lines.join("\n");
     };
+
+    public printFields = async <Data extends object = any>(
+        fields: IField[],
+        data: Data,
+        payload: Record<string, any> = {},
+    ) => {
+        this.loggerService.log("markdownHelperService printFields", {
+            fields,
+            data,
+        });
+        const content = this.buildMarkdownFromFields(fields, data, payload);
+        await downloadMarkdown(content);
+    }
 }
 
 export default MarkdownHelperService;
